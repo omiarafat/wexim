fe1bee27e2f5efab0cb1c84395d373d27394ae6b
omiarafat <arafatislamomino1@gmail.com>
2024-01-02 21:08:39 +0600

    updat page

--- js/script.js
+++ js/script.js
@@ -79,3 +79,12 @@
         clickable: true,
       },
     });
+
+
+    var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
