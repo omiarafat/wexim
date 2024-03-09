467ba0bcd29cbc4da5ef8b628abeab433e7de4d0
ArafatOmi <arafatislamomino1@gmail.com>
2024-03-09 21:11:47 +0600

    add scroll top

--- js/script.js
+++ js/script.js
@@ -4,41 +4,57 @@ new WOW().init();
 
     // <--------navbar-------->
 
-    window.addEventListener("scroll", function () {
-      var header = document.querySelector("header");
-      header.classList.toggle("sticky", window.scrollY > 0);
-    });
-        // Get all sections that have an ID defined
-    const sections = document.querySelectorAll("section[id]");
+     // Get all sections that have an ID defined
+     const sections = document.querySelectorAll("section[id]");
 
-    // Add an event listener listening for scroll
-    window.addEventListener("scroll", navHighlighter);
+     // Add an event listener listening for scroll
+     window.addEventListener("scroll", navHighlighter);
+ 
+     function navHighlighter() {
+       
+       // Get current scroll position
+       let scrollY = window.pageYOffset;
+       
+       // Now we loop through sections to get height, top and ID values for each
+       sections.forEach(current => {
+         const sectionHeight = current.offsetHeight;
+       
+       
+         const sectionTop = (current.getBoundingClientRect().top + window.pageYOffset) - 50;
+         sectionId = current.getAttribute("id");
+         
+         /*
+         - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+         - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+         */
+         if (
+           scrollY > sectionTop &&
+           scrollY <= sectionTop + sectionHeight
+         ){
+           document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
+         } else {
+           document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
+         }
+       });
+     }
+         window.onscroll = function() {scrollFunction()};
+ 
+     function scrollFunction() {
+       if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+         document.getElementById("navber").style.background = "linear-gradient(to right, #919191 2%, #acacac 82%)";
+         document.getElementById("navber").style.animation = "stickySlideDown 1s both";
+         document.getElementById("navber").style.height = "70px";
+         document.getElementById("navber").style.boxShadow = " 0 10px 10px -10px rgba(0, 0, 0, 0.35)";
+         document.getElementById("topArrow").style.display = "block";
+       } else {
+         document.getElementById("navber").style.background = "transparent";
+         document.getElementById("navber").style.boxShadow = "none";
+         document.getElementById("navber").style.height = "90px";
+         document.getElementById("navber").style.animation = "stickySlideDown 1s both";
+         document.getElementById("topArrow").style.display = "none";
+       }
+     }
 
-    function navHighlighter() {
-      
-      // Get current scroll position
-      let scrollY = window.pageYOffset;
-      
-      // Now we loop through sections to get height, top and ID values for each
-      sections.forEach(current => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute("id");
-        
-        /*
-        - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
-        - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
-        */
-        if (
-          scrollY > sectionTop &&
-          scrollY <= sectionTop + sectionHeight
-        ){
-          document.querySelector(".navigation a[href*=" + sectionId + "]").classList.add("active");
-        } else {
-          document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
-        }
-      });
-    }
   let sidebar = document.querySelector('.mobmenu');
   let sidebarTrigger = document.querySelector('.sidemenu-btn');
   let overlay = document.querySelector('.body-overlay');
@@ -143,3 +159,14 @@ new WOW().init();
     // count js end
 
 
+
+    var topArrow = document.querySelector("#topArrow");
+    topArrow.addEventListener("click", scrollToTop);
+    
+    function scrollToTop() {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+      });
+    }
+    
