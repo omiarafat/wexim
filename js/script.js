609e68906f6c97bba6611a050003bdbe3b735df7
ArafatOmi <arafatislamomino1@gmail.com>
2024-06-03 14:28:14 +0600

    uplode

--- js/script.js
+++ js/script.js
@@ -132,6 +132,49 @@ new WOW().init();
     
 
     // count js
+
+
+  //   function visible(partial) {
+  //     var $t = partial,
+  //         $w = jQuery(window),
+  //         viewTop = $w.scrollTop(),
+  //         viewBottom = viewTop + $w.height(),
+  //         _top = $t.offset().top,
+  //         _bottom = _top + $t.height(),
+  //         compareTop = partial === true ? _bottom : _top,
+  //         compareBottom = partial === true ? _top : _bottom;
+  
+  //     return ((compareBottom <= viewBottom) && (compareTop >= viewTop) && $t.is(':visible'));
+  
+  // }
+  
+  // $(window).scroll(function(){
+  
+  //   if(visible($('.count')))
+  //     {
+  //       if($('.count').hasClass('counter-loaded')) return;
+  //       $('.count').addClass('counter-loaded');
+        
+  // $('.count').each(function () {
+  //   var $this = $(this);
+  //   jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+  //     duration: 5000,
+  //     easing: 'swing',
+  //     step: function () {
+  //       $this.text(Math.ceil(this.Counter));
+  //     }
+  //   });
+  // });
+  //     }
+  // })
+  
+      
+
+
+
+
+
+    
     $(document).ready(function() {
 
       var counters = $(".count");
@@ -156,6 +199,8 @@ new WOW().init();
         count(25, counter[j], j);
       }
     });
+    
+    
     // count js end
 
 
